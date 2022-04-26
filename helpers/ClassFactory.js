f2d4eba6b92076c5aa7b9ea23b3a2e1920587d1c
gabriel.nicora <user@tomass-MacBook-Pro.local>
2022-04-26 15:03:38 -0300

    refactor classfactory

--- helpers/ClassFactory.js
+++ helpers/ClassFactory.js
@@ -1,3 +1,4 @@
+import AppScreen from '../test/page-objects/App.screen'
 import listSelectors from '../test/page-objects/listSelectors'
 
 const pickSelectorsByClassName = name => listSelectors[name]
@@ -29,3 +30,20 @@ export const FindElement = name => {
   return createObject(elementSelectors[element])
 }
 
+export const ClassFactory = name => {
+  
+  var [classname, method] = name.split('.')
+
+  var pageSelectors = pickSelectorsByClassName(classname)
+
+  if (pageSelectors[method] == null) {
+
+    if (driver.isAndroid == true) {
+      method += "_Android"
+    } else {
+      method += "_iOS"
+    }
+  }
+
+  return [new AppScreen(pageSelectors), method]
+}
